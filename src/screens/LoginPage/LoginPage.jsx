771893e0c79b54895a875560702c86524bbb0a05
Ethernal1337 <118488974+Ethernal1337@users.noreply.github.com>
2023-07-02 22:59:10 +0300

    Initial commit

---
+++ src/screens/LoginPage/LoginPage.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import "./style.css";
+
+export const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <div className="div">
+        <div className="div-2">
+          <h1 className="NMT">
+            <span className="text-wrapper">N</span>
+            <span className="span">M</span>
+            <span className="text-wrapper-2">T</span>
+          </h1>
+          <div className="div-2" />
+          <div className="div-3">
+            <span className="text-wrapper-3">Вход </span>
+            <span className="text-wrapper-4">|</span>
+            <span className="text-wrapper-3"> Регистрация</span>
+          </div>
+          <div className="NMT">
+            <span className="text-wrapper">N</span>
+            <span className="span">M</span>
+            <span className="text-wrapper-2">T</span>
+          </div>
+        </div>
+        <div className="overlap">
+          <div className="rectangle" />
+          <img
+            className="discord"
+            alt="Discord"
+            src="https://generation-sessions.s3.amazonaws.com/6d9d38ff43e177ee6e117fb5db22273c/img/discord-1@2x.png"
+          />
+          <img
+            className="VK-com-logo"
+            alt="Vk com logo"
+            src="https://generation-sessions.s3.amazonaws.com/6d9d38ff43e177ee6e117fb5db22273c/img/vk-com-logo-2@2x.png"
+          />
+          <img
+            className="telegram"
+            alt="Telegram"
+            src="https://generation-sessions.s3.amazonaws.com/6d9d38ff43e177ee6e117fb5db22273c/img/telegram-1-1.svg"
+          />
+          <div className="text-wrapper-5">Все права защищены.</div>
+          <div className="text-wrapper-6">Условия использования</div>
+          <div className="text-wrapper-7">Политика конфендециальности</div>
+          <p className="p">Авторское право © 2023 NMT Inc.</p>
+          <div className="rectangle-2" />
+          <div className="rectangle" />
+          <img
+            className="discord"
+            alt="Discord"
+            src="https://generation-sessions.s3.amazonaws.com/6d9d38ff43e177ee6e117fb5db22273c/img/discord-1@2x.png"
+          />
+          <img
+            className="VK-com-logo"
+            alt="Vk com logo"
+            src="https://generation-sessions.s3.amazonaws.com/6d9d38ff43e177ee6e117fb5db22273c/img/vk-com-logo-2@2x.png"
+          />
+          <img
+            className="telegram"
+            alt="Telegram"
+            src="https://generation-sessions.s3.amazonaws.com/6d9d38ff43e177ee6e117fb5db22273c/img/telegram-1-2.svg"
+          />
+          <div className="text-wrapper-5">Все права защищены.</div>
+          <div className="text-wrapper-6">Условия использования</div>
+          <div className="text-wrapper-7">Политика конфендециальности</div>
+          <p className="p">Авторское право © 2023 NMT Inc.</p>
+        </div>
+        <div className="overlap-group">
+          <div className="overlap-2">
+            <div className="div-wrapper">
+              <div className="text-wrapper-8">Login</div>
+            </div>
+            <div className="text-wrapper-9">Login</div>
+            <div className="text-wrapper-10">Name</div>
+            <div className="rectangle-3" />
+            <div className="text-wrapper-11">Password</div>
+            <p className="don-t-have-on">
+              <span className="text-wrapper-12">Don’t have on account? </span>
+              <span className="text-wrapper-13">Register</span>
+            </p>
+            <div className="rectangle-4" />
+          </div>
+          <div className="rectangle-5" />
+        </div>
+      </div>
+    </div>
+  );
+};
